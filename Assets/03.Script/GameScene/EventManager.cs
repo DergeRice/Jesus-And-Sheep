f1b5e6bd7becc34f349c15bdd845d0585b2f857e
DergeRice <114036224+DergeRice@users.noreply.github.com>
2024-11-27 08:09:23 +0900

    umjunpal

--- Assets/03.Script/GameScene/EventManager.cs
+++ Assets/03.Script/GameScene/EventManager.cs
@@ -4,27 +4,9 @@ using UnityEngine.UI;
 public class EventManager : MonoBehaviour
 {
     public Texture commonTexture;
-    public GameEvent 공5개_추가_다음3턴_양체력2배;
-    public GameEvent 가장_앞줄양들_전부구원_공5개감소;
-    public GameEvent 공_절반만_남기고_전부_구원;
-    public GameEvent 화면에있는_모든양들_체력2배_공3개추가;
-    public GameEvent 랜덤_10개_블록_체력_2배_공5개추가;
-    public GameEvent 한턴쉬고_공5개추가;
-    public GameEvent 두턴쉬고_공10개추가;
-    public GameEvent 세턴쉬고_공15개추가;
-    public GameEvent 랜덤_한개_블록_체력_3배_공3개추가;
-    public GameEvent 체력30미만_양들_즉시구원_공1개감소;
-    public GameEvent 체력100미만_양들_즉시구원_공10개감소;
-    public GameEvent 모든애들에게50데미지_공5개감소;
-    public GameEvent 모든애들에게10데미지_공1개감소;
-    public GameEvent 랜덤3개블록_100데미지_공3개감소;
-    public GameEvent 랜덤으로블록5개설치_공5개추가;
-    public GameEvent 랜덤으로블록10개설치_랜덤특수공1개추가;
-    public GameEvent 반반확률로_공5추가_공2감소;
-    public GameEvent 반반확률로_공5추가_블록3개소환;
-    public GameEvent 반반확률로_특수공1추가_공2감소;
-    public GameEvent 반반확률로_특수공1추가_블록5개소환;
+
     public int zaebal;
+
 
     private void Start()
     {
